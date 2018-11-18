6fb93c595f47650ba8f3462a9c2bc7c2ba9e9108
Aditya Singh <morfsys@gmail.com>
2018-11-18 18:32:33 +0530

    Final commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { View, Text, TouchableOpacity, Modal, WebView } from "react-native";
+
+export default class App extends React.Component {
+    state = {
+        showModal: false,
+        status: "Pending"
+    };
+    handleResponse = data => {
+        if (data.title === "success") {
+            this.setState({ showModal: false, status: "Complete" });
+        } else if (data.title === "cancel") {
+            this.setState({ showModal: false, status: "Cancelled" });
+        } else {
+            return;
+        }
+    };
+    render() {
+        return (
+            <View style={{ marginTop: 100 }}>
+                <Modal
+                    visible={this.state.showModal}
+                    onRequestClose={() => this.setState({ showModal: false })}
+                >
+                    <WebView
+                        source={{ uri: "http://localhost:3000" }}
+                        onNavigationStateChange={data =>
+                            this.handleResponse(data)
+                        }
+                        injectedJavaScript={`document.f1.submit()`}
+                    />
+                </Modal>
+                <TouchableOpacity
+                    style={{ width: 300, height: 100 }}
+                    onPress={() => this.setState({ showModal: true })}
+                >
+                    <Text>Pay with Paypal</Text>
+                </TouchableOpacity>
+                <Text>Payment Status: {this.state.status}</Text>
+            </View>
+        );
+    }
+}
